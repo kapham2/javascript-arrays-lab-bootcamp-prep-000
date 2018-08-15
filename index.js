fb0a4c2fcfa5e024b59caaeacc758b4d006c728f
Kim Pham <pham.kim.a@gmail.com>
2018-08-15 23:02:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions and code here
+function destructivelyAppendKitten(name)
+{
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name)
+{
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten()
+{
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten()
+{
+  kittens.shift()
+}
+
+function appendKitten(name)
+{
+  return kitten.concat(name)
+}
